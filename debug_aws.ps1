afd8ac8a479b4c8da0247b8b590318890b5ab8fd
Juan Pablo Osorio <contacto@simplaw.co>
2026-02-23 19:13:56 -0500

    chore: add docker compose logs to deployment script for debugging

---
+++ debug_aws.ps1
@@ -0,0 +1,7 @@
+param(
+    [string]$path_to_key = "..\simplaw.pem",
+    [string]$aws_host = "ec2-user@your-ec2-ip.compute.amazonaws.com"
+)
+
+# This script is a placeholder since we don't have the user's secret AWS key locally.
+# We will instead create a debug workflow in GitHub Actions to dump the logs for us to read from out here.
